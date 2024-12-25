195b165739db4a976ec02849880720013d884744
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-12-26 00:25:37 +0900

    first commit

---
+++ src/test/java/hackerton/saver/SaverApplicationTests.java
@@ -0,0 +1,15 @@
+package hackerton.saver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class SaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
